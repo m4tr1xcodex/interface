fa48a18503eec7a5dc2776076549039480a1228b
Manuel Lopez <ma3xcodes@gmail.com>
2024-01-22 14:44:41 -0600

    Add status on get next

--- pages/api/groups/next.tsx
+++ pages/api/groups/next.tsx
@@ -5,29 +5,30 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-    try{
-        const getGroup = await prisma.group.findFirst({
-            where: {
-                finished: false,
-                started: false
-            },
-        });
-        
-        if(!getGroup){
-            return res.status(404).json({
-                message: "No more groups found"
-            });
-        }
-        const updatedGroup = await prisma.group.update({
-            where: { id: getGroup?.id },
-            data: { started : true }
-        });
-        return res.status(200).json(updatedGroup);
-    }catch(err:any){
-        console.log(err)
-        console.log('keys: ', Object.keys(err));
-        console.log('error.errorCode: ', err?.errorCode);
-        console.error(JSON.stringify(err, null, 2));
-        return res.status(500);
+  try {
+    const getGroup = await prisma.group.findFirst({
+      where: {
+        finished: false,
+        started: false,
+      },
+    });
+
+    if (!getGroup) {
+      return res.status(404).json({
+        status: "error",
+        message: "No more groups found",
+      });
     }
+    const updatedGroup = await prisma.group.update({
+      where: { id: getGroup?.id },
+      data: { started: true },
+    });
+    return res.status(200).json(updatedGroup);
+  } catch (err: any) {
+    console.log(err);
+    console.log("keys: ", Object.keys(err));
+    console.log("error.errorCode: ", err?.errorCode);
+    console.error(JSON.stringify(err, null, 2));
+    return res.status(500);
+  }
 }
